e74234e0fd5c59450c2d5032d0ed6088590fda3a
Adrien Merignac <adrien.merignac92@gmail.com>
2025-03-12 15:10:18 +0100

    Add auto scroll to carousel

--- src/app/components/top-animes/top-animes.component.ts
+++ src/app/components/top-animes/top-animes.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MangaService } from '../../services/manga.service';
@@ -10,9 +10,10 @@ import { MangaService } from '../../services/manga.service';
   templateUrl: './top-animes.component.html',
   styleUrls: ['./top-animes.component.scss'],
 })
-export class TopAnimesComponent implements OnInit {
+export class TopAnimesComponent implements OnInit, OnDestroy {
   animes: any[] = [];
   currentSlide = 0;
+  autoScrollInterval: any; // ✅ Store the interval for auto-scroll
 
   constructor(private mangaService: MangaService) {}
 
@@ -20,25 +21,43 @@ export class TopAnimesComponent implements OnInit {
     this.loadAnimes();
   }
 
+  ngOnDestroy() {
+    clearInterval(this.autoScrollInterval); // ✅ Stop auto-scroll when component is destroyed
+  }
+
   loadAnimes() {
     this.mangaService.getTopAnimes(1).subscribe((data) => {
       this.animes = data.data;
+      this.startAutoScroll(); // ✅ Start auto-scroll once data is loaded
     });
+  }
+
+  startAutoScroll() {
+    this.autoScrollInterval = setInterval(() => {
+      this.nextSlide();
+    }, 10000); // ✅ Auto-scroll every 5 seconds
+  }
+
+  resetAutoScroll() {
+    clearInterval(this.autoScrollInterval);
+    this.startAutoScroll(); // ✅ Restart the timer after user interaction
   }
 
   nextSlide() {
     if (this.currentSlide < this.animes.length - 1) {
       this.currentSlide++;
     } else {
-      this.currentSlide = 0; // ✅ Revient au début si on est à la fin
+      this.currentSlide = 0; // ✅ Loop back to the first slide
     }
+    this.resetAutoScroll();
   }
 
   prevSlide() {
     if (this.currentSlide > 0) {
       this.currentSlide--;
     } else {
-      this.currentSlide = this.animes.length - 1; // ✅ Revient à la fin si on est au début
+      this.currentSlide = this.animes.length - 1; // ✅ Loop back to the last slide
     }
+    this.resetAutoScroll();
   }
 }
